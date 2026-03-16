43f735f4284da66d6ad3f8f618f167781a62e9e3
Sebastian Furfaro <seba.furfaro@gmail.com>
2026-03-16 09:23:30 -0300

    add documentation link

--- app/plataforma/[tenantId]/panel/components/layout/Aside.tsx
+++ app/plataforma/[tenantId]/panel/components/layout/Aside.tsx
@@ -417,7 +417,7 @@ export function Aside({
             <>
               <SectionTitle label="Ayuda" isCollapsed={effectiveIsCollapsed} />
               <NavItem
-                href={`${base}/documentacion`}
+                href={"https://amethyst-pulsar-ff6.notion.site/Te-damos-la-bienvenida-a-Turnos-Nodo-App-3234a9e6636f80b8b740c5ff591a02cf"}
                 label="Documentación"
                 icon={<BookOpen className="w-5 h-5" />}
                 isCollapsed={effectiveIsCollapsed}
